c14f89092fcc84f33f55c7128fd4fec11c9c4010
KandukuriAmar <2200030959@kluniversity.in>
2024-12-17 01:28:15 +0530

    contact json to string modified

--- src/Components/Contactme.jsx
+++ src/Components/Contactme.jsx
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './Header';
 export default function Contactme({mode, togglemode}) {
-  // const [data, setData] = useState({
-  //   name: "",
-  //   email: "",
-  //   message: ""
-  // });
-
-  // const handleStore = (e) =>{
-  //   setData({...data, [e.target.name]: e.target.value});
-  // }
-  const [text, setText] = useState("");
-
-  const handleSubmit = async(e) =>{
-    // e.preventDefault();
-    // setData({name: '', email: '', message: ''});
-    setText("Thanks for contacting me");
-    setTimeout(()=>{
-      setText("");
-    },3000);
-  }
-
   return (
     <>
      <>
       <Header mode={mode} togglemode={togglemode}/>
       <div className= "flex justify-center items-center min-h-screen flex-row">
-        <form 
-          onSubmit={handleSubmit}
-          // method="POST"
+        <div 
           className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md"
         >
           <div className="mb-9 text-2xl text-black flex items-center justify-center">Contact Me</div>
@@ -37,8 +15,7 @@ export default function Contactme({mode, togglemode}) {
             <input 
               name="name" 
               type="text" 
-              // value={data.name}
-              // onChange={handleStore} 
+              
               required
               className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-900" 
             />
@@ -49,8 +26,7 @@ export default function Contactme({mode, togglemode}) {
             <input 
               name="email" 
               type="email" 
-              // value={data.email}
-              // onChange={handleStore}
+              
               required
               className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-700" 
             />
@@ -60,8 +36,7 @@ export default function Contactme({mode, togglemode}) {
             <label className="block text-slate-900 font-semibold">Message:</label>
             <textarea 
               name="message" 
-              // value={data.message}
-              // onChange={handleStore} 
+              
               required
               className="w-full mt-1 p-2 border rounded-md text-black focus:outline-none focus:ring-2 focus:ring-slate-700" 
             />
@@ -74,9 +49,7 @@ export default function Contactme({mode, togglemode}) {
               className="w-full bg-zinc-700 text-white font-semibold py-2 rounded-md hover:bg-card cursor-pointer" 
             />
           </div>
-
-          <p className='text-xl text-slate-900 flex items-center justify-center py-2'>{text}</p>
-        </form>
+        </div>
       </div>
      </> 
     </>
