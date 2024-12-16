e7fc32e34baf73a78b8269e0ca148b2da61bd4cc
KandukuriAmar <2200030959@kluniversity.in>
2024-12-17 01:46:47 +0530

    contact json to string modified

--- src/Components/Contactme.jsx
+++ src/Components/Contactme.jsx
@@ -1,65 +1,65 @@
-import React from 'react';
-import Header from './Header';
-export default function Contactme({mode, togglemode}) {
-  return (
-    <>
-     <>
-      <Header mode={mode} togglemode={togglemode}/>
-      <div className= "flex justify-center items-center min-h-screen flex-row">
-        <div 
-          className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md"
-        >
-          <div className="mb-9 text-2xl text-black flex items-center justify-center">Contact Me</div>
-          <div className="mb-4">
-            <label className="block text-slate-900 font-semibold">Name:</label>
-            <input 
-              name="name" 
-              type="text" 
-              
-              required
-              className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-900" 
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-slate-900 font-semibold">Email:</label>
-            <input 
-              name="email" 
-              type="email" 
-              
-              required
-              className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-700" 
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-slate-900 font-semibold">Message:</label>
-            <textarea 
-              name="message" 
-              
-              required
-              className="w-full mt-1 p-2 border rounded-md text-black focus:outline-none focus:ring-2 focus:ring-slate-700" 
-            />
-          </div>
-
-          <div>
-            <input 
-              type="submit" 
-              value="Submit" 
-              className="w-full bg-zinc-700 text-white font-semibold py-2 rounded-md hover:bg-card cursor-pointer" 
-            />
-          </div>
-        </div>
-      </div>
-     </> 
-    </>
-  )
-}
-
+// import React from 'react';
+// import Header from './Header';
 // export default function Contactme({mode, togglemode}) {
 //   return (
-//   <>
-//   <h1>Contactme</h1>
+//     <>
+//      <>
+//       <Header mode={mode} togglemode={togglemode}/>
+//       <div className= "flex justify-center items-center min-h-screen flex-row">
+//         <div 
+//           className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md"
+//         >
+//           <div className="mb-9 text-2xl text-black flex items-center justify-center">Contact Me</div>
+//           <div className="mb-4">
+//             <label className="block text-slate-900 font-semibold">Name:</label>
+//             <input 
+//               name="name" 
+//               type="text" 
+              
+//               required
+//               className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-900" 
+//             />
+//           </div>
+
+//           <div className="mb-4">
+//             <label className="block text-slate-900 font-semibold">Email:</label>
+//             <input 
+//               name="email" 
+//               type="email" 
+              
+//               required
+//               className="w-full mt-1 p-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-slate-700" 
+//             />
+//           </div>
+
+//           <div className="mb-4">
+//             <label className="block text-slate-900 font-semibold">Message:</label>
+//             <textarea 
+//               name="message" 
+              
+//               required
+//               className="w-full mt-1 p-2 border rounded-md text-black focus:outline-none focus:ring-2 focus:ring-slate-700" 
+//             />
+//           </div>
+
+//           <div>
+//             <input 
+//               type="submit" 
+//               value="Submit" 
+//               className="w-full bg-zinc-700 text-white font-semibold py-2 rounded-md hover:bg-card cursor-pointer" 
+//             />
+//           </div>
+//         </div>
+//       </div>
+//      </> 
 //     </>
-//   );
+//   )
 // }
+
+export default function Contactme({mode, togglemode}) {
+  return (
+  <>
+  <h1>Contactme</h1>
+    </>
+  );
+}
